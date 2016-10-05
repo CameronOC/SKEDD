96a2768933d61a3ff23115ec4ba5805be0632903
Philip Guther <philip@guther.com>
2016-10-05 15:30:43 -0700

    removed flask development toolbar for now

--- source/__init__.py
+++ source/__init__.py
@@ -8,11 +8,11 @@
 import os
 
 from flask import Flask, render_template
-from flask.ext.login import LoginManager
-from flask.ext.bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from flask_mail import Mail
-from flask.ext.debugtoolbar import DebugToolbarExtension
-from flask.ext.sqlalchemy import SQLAlchemy
+# from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
 
 
 ################
@@ -31,7 +31,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 bcrypt = Bcrypt(app)
 mail = Mail(app)
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 db = SQLAlchemy(app)
 
 
