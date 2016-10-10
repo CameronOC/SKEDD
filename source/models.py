4bbc0c6c8a69fcd7e232ec0d8964f1dbf473fa13
Philip Guther <philip@guther.com>
2016-10-10 14:15:44 -0700

    more work on adding a new organization

--- source/models.py
+++ source/models.py
@@ -8,8 +8,6 @@ from source import db, bcrypt
 
 class User(db.Model):
 
-    __tablename__ = "user"
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
@@ -41,9 +39,11 @@ class User(db.Model):
 
 class Organization(db.Model):
 
-    __tablename__ = "organizations"
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, name, owner):
+        self.name = name
+        self.owner_id = owner.id
 
