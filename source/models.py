b694214208dcef0b4e050bf0891c45c740372ec4
Robert Lyons <rglyons@ucsc.edu>
2016-10-11 20:06:27 -0700

    Shifts model complete

--- source/models.py
+++ source/models.py
@@ -15,10 +15,9 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
-    
+        
     # relationship with shifts
-    shifts = relationship("Shift", backref="user")
-
+    shifts = db.relationship("Shift", backref="user")
 
     def __init__(self, email, password, paid=False, admin=False):
         self.email = email
@@ -39,19 +38,28 @@ class User(db.Model):
         return self.id
 
     def __repr__(self):
-        return '<email {}'.format(self.email)
-
+        return '<email {}>'.format(self.email)
+        
+        
 class Shift(db.Model):
 	
 	__tablename__ = "shifts"
 	
 	id = db.Column(db.Integer, primary_key=True)
-	day = db.Column(db.DateTime, nullable=False)
+	day = db.Column(db.String, nullable=False)
 	start_time = db.Column(db.DateTime, nullable=False)
 	end_time = db.Column(db.DateTime, nullable=False)
-	duration = db.Column(db.Float, nullable=False)
+	duration = db.Column(db.String, nullable=False)
 	
 	# relationship with user
-	assigned_user_id = db.Column(db.Integer, ForeignKey('user.id'))
-
+	assigned_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+	
+	def __init__(self, assigned_user_id, day):
+		self.assigned_user_id = assigned_user_id
+		self.day = day
+		self.start_time = datetime.datetime.now()
+		self.end_time = datetime.datetime.now()
+		#self.start_time = start_time
+		#self.end_time = end_time
+		self.duration = datetime.time(0, 0, (self.end_time - self.start_time).seconds).strftime("%H:%M")
 
