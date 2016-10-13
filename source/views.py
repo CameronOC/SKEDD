ce6424d684b33acfadf7938df16f7c8767bf1f3d
Philip Guther <philip@guther.com>
2016-10-13 10:56:39 -0700

    added decorator to check if user owns organization, will add more as they are needed

--- source/views.py
+++ source/views.py
@@ -51,7 +51,6 @@ def home():
     return render_template('main/home.html', organizations=orgs)
 
 
-
 @main_blueprint.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
@@ -75,6 +74,6 @@ def organization(key):
     org = Organization.query.filter_by(id=key).first()
 
     if org.owner.id != g.user.id:
-        return render_template('errors/403_organization.html'), 403
+        return render_template('errors/403_organization_owner.html'), 403
 
     return render_template('main/organization.html', organization=org)
