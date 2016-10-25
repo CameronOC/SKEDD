b22a816468d924692ca9b5a42e8f0c45c27cab5d
Robert Lyons <rglyons@ucsc.edu>
2016-10-24 19:17:46 -0700

    reformatted models.Shift

--- project/models.py
+++ project/models.py
@@ -81,11 +81,10 @@ class Shift(db.Model):
         self.day = day
         self.start_time = start_time
         self.end_time = end_time
-
-        zero = datetime.datetime.strptime('00:00',
-                                          '%H:%M')  # zero o'clock datetime to add timedelta object to (end_time - start_time)
-        self.duration = zero + (
-        self.end_time - self.start_time)  # this is how we convert from timedelta obj to datetime obj
+        # zero o'clock datetime to add timedelta object to (end_time - start_time)
+        zero = datetime.datetime.strptime('00:00','%H:%M')
+        # this is how we convert from timedelta obj to datetime obj  
+        self.duration = zero + (self.end_time - self.start_time)  
 
 
 class Membership(db.Model):
