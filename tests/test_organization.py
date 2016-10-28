7e44d2ba7154c274a8ed1719bf25ac32899d4e8e
Robert Pence <rpence@ucsc.edu>
2016-10-28 11:49:17 -0700

    wrote test for assign_member_to_position

--- tests/test_organization.py
+++ tests/test_organization.py
@@ -1,7 +1,7 @@
 from flask_testing import TestCase
 from flask import g
 from project import app, db
-from project.models import User, Organization, Membership, Position, Shift
+from project.models import User, Organization, Membership, Position, Shift, position_assignments
 import project.utils.organization as org_utils
 #from project.utils.organization import deletepositions
 # from project.utils.organization import create_organization, get_organization
@@ -215,7 +215,18 @@ class TestOrganization(TestCase):
         Tests that a user is assigned to a position
         :return:
         """
-
+        pos = org_utils.get_position(1)
+        org = org_utils.get_organization(1)
+        user = User(
+            email='member2@organization.com',
+            first_name='Will',
+            last_name='Smith',
+            password='password',
+            confirmed=True
+        )
+        org_utils.assign_member_to_position(user, pos, org)
+        assigneduser = position_assignments.select(position_assignments.c.user_id == 2)
+        assert assigneduser is not None
 
     def test_unassign_member_to_position(self):
         """
