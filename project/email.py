d29779476a9c326d1b68ea527f5e9d8e3f49442e
Cameron johnson <cojohnso@ucsc.edu>
2016-11-17 14:29:00 -0800

    took out email usage for user email

--- project/email.py
+++ project/email.py
@@ -10,11 +10,8 @@ from project import app, mail
 
 def send_email(to, subject, template):
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-    from_email = Email('APP_EMAIL_USERNAME')
-    subject = "Hello World from the SendGrid Python Library!"
-    to_email = Email("cjplanes@gmail.com")
-    content = Content("text/plain", "Hello, Email!")
-    mail = Mail(from_email, subject, to_email, content)
+    from_email = Email("skedd.mail@gmail.com")
+    mail = Mail(from_email, subject, to, template)
     response = sg.client.mail.send.post(request_body=mail.get())
     print(response.status_code)
     #msg = Message(
