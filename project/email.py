15aa9083d7ae5aff430bc0e64ef655f6d8b38f70
Cameron johnson <cojohnso@ucsc.edu>
2016-11-17 13:37:55 -0800

    editted content of email

--- project/email.py
+++ project/email.py
@@ -10,8 +10,9 @@ from project import app, mail
 
 def send_email(to, subject, template):
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-    from_email = os.environ['APP_MAIL_USERNAME']
-    mail = Mail(from_email, subject, to, template)
+    from_email = Email(os.environ['APP_MAIL_USERNAME'])
+    content = Content("text/plain", "Hello, Email!")
+    mail = Mail(from_email, subject, to, content)
     response = sg.client.mail.send.post(request_body=mail.get())
     print(response.status_code)
     print(response.body)
