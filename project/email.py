dab156299823920217c77006d5de5744b5fab693
Cameron johnson <cojohnso@ucsc.edu>
2016-11-17 14:07:25 -0800

    changed sender of email

--- project/email.py
+++ project/email.py
@@ -10,7 +10,7 @@ from project import app, mail
 
 def send_email(to, subject, template):
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-    from_email = Email(os.environ['APP_MAIL_USERNAME'])
+    from_email = Email(os.environ['SENDGRID_USERNAME'])
     content = Content("text/plain", "Hello, Email!")
     mail = Mail(from_email, subject, to, content)
     response = sg.client.mail.send.post(request_body=mail.get())
