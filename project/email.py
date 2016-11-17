1c036df5a1d79cc0052da530b35456d505522825
Cameron johnson <cojohnso@ucsc.edu>
2016-11-17 13:25:10 -0800

    heroku testing branch

--- project/email.py
+++ project/email.py
@@ -1,15 +1,25 @@
 # project/email.py
 
 from flask.ext.mail import Message
+import os
+import sendgrid
+from sendgrid.helpers.mail import *
 
 from project import app, mail
 
 
 def send_email(to, subject, template):
-    msg = Message(
-        subject,
-        recipients=[to],
-        html=template,
-        sender=app.config['MAIL_DEFAULT_SENDER']
-    )
-    mail.send(msg)
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = os.environ['APP_MAIL_USERNAME']
+    mail = Mail(from_email, subject, to, template)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+    #msg = Message(
+     #   subject,
+      #  recipients=[to],
+       # html=template,
+        #sender=app.config['MAIL_DEFAULT_SENDER']
+    #)
+    #mail.send(msg)
