810bcb72ffb18355cc689651a041157eb1cbe426
Cameron johnson <cojohnso@ucsc.edu>
2016-11-17 14:46:03 -0800

    Mail V2 instead of V3

--- project/email.py
+++ project/email.py
@@ -14,8 +14,8 @@ def send_email(to, subject, template):
 
     message.add_to("cjplanes@gmail.com")
     message.set_from("skedd.mail@gmail.com")
-    message.set_subject(subject)
-    message.set_html(template)
+    message.set_subject("Sending with SendGrid is Fun")
+    message.set_html("and easy to do anywhere, even with Python")
 
     sg.send(message)
     #msg = Message(
