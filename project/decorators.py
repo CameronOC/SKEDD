454afd3cfcf81841e77871b09bd89db7bcea1623
Philip Guther <philip@guther.com>
2016-11-17 01:54:18 -0800

    added a new decorator to check if a shift belongs to the organization given

--- project/decorators.py
+++ project/decorators.py
@@ -6,8 +6,8 @@ from functools import wraps
 from flask import flash, redirect, url_for, abort, render_template, g
 from flask_login import current_user
 
-from models import Organization, Membership
-from exceptions import NotOwner, NotMember
+from models import Organization, Membership, Shift
+from exceptions import NotOwner, NotMember, ShiftNotInOrg
 
 
 def check_confirmed(func):
@@ -50,10 +50,21 @@ def organization_member(fn):
     @wraps(fn)
     def decorated_view(*args, **kwargs):
         key = kwargs['key']
-        print key
         membership = Membership.query.filter_by(member_id=g.user.id, organization_id=key).first()
 
         if membership is None:
             raise NotMember('403 Access Denied. You must be a member of this organization.', status_code=403)
+        return fn(*args, **kwargs)
+    return decorated_view
+
+def shift_belongs_to_org(fn):
+    @wraps(fn)
+    def decorated_view(*args, **kwargs):
+        key = kwargs['key']
+        key1 = kwargs['key1']
+
+        shift = Shift.query.get(key1)
+        if shift is None or shift.Position.organization_id != key:
+            raise ShiftNotInOrg('400 Bad Request. No such Shift for this Organization', status_code=404)
         return fn(*args, **kwargs)
     return decorated_view
--- project/exceptions.py
+++ project/exceptions.py
@@ -1,6 +1,7 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, Response
 from project import app
 from flask import request
+import json
 
 def request_wants_json():
     best = request.accept_mimetypes \
@@ -43,6 +44,22 @@ class NotMember(Exception):
         rv['message'] = self.message or '403 Access Denied. You must be a member of this organization.'
         return rv
 
+class ShiftNotInOrg(Exception):
+    status_code = 403
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['status'] = 'error'
+        rv['message'] = self.message or '400 Bad Request. No such Shift for this Organization'
+        return rv
+
 
 @app.errorhandler(NotOwner)
 def handle_invalid_usage(error):
@@ -61,3 +78,25 @@ def handle_invalid_usage(error):
         return response
     else:
         return render_template('errors/403_organization_member.html'), 403
+
+@app.errorhandler(ShiftNotInOrg)
+def handle_invalid_usage(error):
+    if request_wants_json():
+        response = jsonify(error.to_dict())
+        response.status_code = error.status_code
+        return response
+    else:
+        return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def handle_internal_error(error):
+    if request_wants_json():
+        response_dict = {
+            'status': 'error',
+            'message': 'An internal server error occured. Please try again later.'
+        }
+        return Response(response=json.dumps(response_dict),
+                        status=200,
+                        mimetype="application/json")
+    else:
+        return render_template('errors/500.html'), 500
