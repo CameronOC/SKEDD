7b1537b5364fe659fe2ce083e36ea449ad67570e
Cameron Johnson <cjplanes@gmail.com>
2016-11-17 17:02:09 -0800

    Sengrid test

--- project/email.py
+++ project/email.py
@@ -11,9 +11,8 @@ from project import app, mail
 
 def send_email(to, subject, template):
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-    from_email = Email("test@example.com")
+    from_email = Email("skedd.mail@gmail.com")
     to_email = Email(to)
-    subject = "Sending with SendGrid is Fun"
     content = Content("text/plain", "and easy to do anywhere, even with Python")
     mail = Mail(from_email, subject, to_email, content)
     response = sg.client.mail.send.post(request_body=mail.get())
