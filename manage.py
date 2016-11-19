537e2e927903d6e78d5ca05420ccd2bcbbabce98
Cameron Johnson <cojohnso@ucsc.edu>
2016-11-18 17:28:59 -0800

    Directory for migrate changed

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ from project.models import User
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db,directory='migrations')
 manager = Manager(app)
 
 # migrations
